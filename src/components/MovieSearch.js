3fcc36c771d487369cd758f907acf53e8576f7e6
Brett Commandeur <bcmdr.co@gmail.com>
2019-11-17 20:02:54 -0700

    fix moviedbapi key import

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,9 +1,8 @@
-import tmdb from '../config/tmdb'
-
 import React, { useState } from 'react';
 
 const MovieSearch = ({setMovieListSource}) => {
   const [searchValue, setSearchValue] = useState('');
+  const movieDbApiKey = process.env.movieDbApiKey
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
@@ -16,7 +15,7 @@ const MovieSearch = ({setMovieListSource}) => {
     // const configResponse = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${tmdb.key}`)
     // const config = await configResponse.json();
     
-    const resultsResponse = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${tmdb.key}&query=${searchValue}&page=1`);
+    const resultsResponse = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${movieDbApiKey}&query=${searchValue}&page=1`);
     const results = await resultsResponse.json();
 
     console.log(results)
