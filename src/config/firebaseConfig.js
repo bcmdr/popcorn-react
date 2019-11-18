222d46e7516b2a46d12b7d701144c71d57714e95
Brett Commandeur <bcmdr.co@gmail.com>
2019-11-17 19:59:19 -0700

    fix firebase import error

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCvsBUVakhVDqCZkK0B-_X1dwpZruodew",
+  authDomain: "popcorn-next.firebaseapp.com",
+  databaseURL: "https://popcorn-next.firebaseio.com",
+  projectId: "popcorn-next",
+  storageBucket: "popcorn-next.appspot.com",
+  messagingSenderId: "95146455879",
+  appId: "1:95146455879:web:16eab5043b5a9781b69262",
+  measurementId: "G-W9P0NX8Y51"
+};
+
+export default firebaseConfig;
